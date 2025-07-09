27a25992616286a66b65593ad0a0543b5463f44a
Samira <samirahekmati@gmail.com>
2025-07-09 09:53:55 +0100

    feat: add authRouter to main API routes under /auth

--- api/api.js
+++ api/api.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 
 import messageRouter from "./messages/messageRouter.js";
+import authRouter from "./auth/authRouter.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
+api.use("/auth", authRouter)
+
 
 export default api;
