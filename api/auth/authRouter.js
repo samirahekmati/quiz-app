f8310412870e4faba1973f8d987ead30c4792007
Samira <samirahekmati@gmail.com>
2025-07-09 09:54:25 +0100

    feat: create authRouter with /login and /register endpoints

---
+++ api/auth/authRouter.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { handleLogin, handleRegister } from "./authController.js";
+
+
+
+const router = Router();
+
+router.post("/login", handleLogin)
+router.post('/register', handleRegister);
+
+export default router;
