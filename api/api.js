6e76a187f24222c8c3d9d358a99f1e8e282ff6b8
Samira <samirahekmati@gmail.com>
2025-07-11 19:57:56 +0100

    feat(api): wire up `/quizzes` route to the main API router

--- api/api.js
+++ api/api.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import authRouter from "./auth/authRouter.js";
 import messageRouter from "./messages/messageRouter.js";
+import quizRouter from "./quiz/quizRouter.js";
 
 const api = Router();
 
 api.use("/message", messageRouter);
 api.use("/auth", authRouter);
+api.use("/quizzes", quizRouter);
 
 export default api;
