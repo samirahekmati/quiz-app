273a48ee21f28e4e2712186c04d206e653c30b65
Samira <samirahekmati@gmail.com>
2025-07-12 13:57:27 +0100

    feat: add `questions` and `options` table definition with constraints and defaults

--- documentation/ddl.sql
+++ documentation/ddl.sql
@@ -29,3 +29,37 @@ CREATE TABLE public.quizzes (
 	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
 	CONSTRAINT quizzes_pkey PRIMARY KEY (id)
 );
+
+
+-- public.questions definition
+
+-- Drop table
+
+-- DROP TABLE public.questions;
+
+CREATE TABLE public.questions (
+	id serial4 NOT NULL,
+	quiz_id int4 NULL,
+	"text" text NOT NULL,
+	"type" varchar(50) DEFAULT 'multiple_choice'::character varying NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	difficulty_level public.difficulty_enum NULL,
+	CONSTRAINT questions_pkey PRIMARY KEY (id),
+	CONSTRAINT questions_quiz_id_fkey FOREIGN KEY (quiz_id) REFERENCES public.quizzes(id) ON DELETE CASCADE
+);
+
+-- public."options" definition
+
+-- Drop table
+
+-- DROP TABLE public."options";
+
+CREATE TABLE public."options" (
+	id int4 DEFAULT nextval('answers_id_seq'::regclass) NOT NULL,
+	question_id int4 NULL,
+	"text" text NOT NULL,
+	is_correct bool DEFAULT false NOT NULL,
+	created_at timestamp DEFAULT CURRENT_TIMESTAMP NULL,
+	CONSTRAINT answers_pkey PRIMARY KEY (id),
+	CONSTRAINT answers_question_id_fkey FOREIGN KEY (question_id) REFERENCES public.questions(id) ON DELETE CASCADE
+);
