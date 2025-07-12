aef2c4826ce896e752a53f4a1828f0d7b55df1cf
Samira <samirahekmati@gmail.com>
2025-07-12 14:55:53 +0100

    feat: set up route for creating new quizzes

--- api/quiz/quizRouter.js
+++ api/quiz/quizRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createQuiz } from "./quizController.js";
+import { createQuestion } from "../questions/questionController.js";
 
 const router = Router();
 
 // Route to create a new quiz
 router.post("/", createQuiz);
+router.post("/:quizId/questions", createQuestion);
 
 export default router;
