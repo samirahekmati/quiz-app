c0c525a832a98e3da119c3a51e37f77c63072c2b
Samira <samirahekmati@gmail.com>
2025-07-16 23:01:13 +0100

    Add answerRouter mount and deleteQuiz route to quizRouter

--- api/quiz/quizRouter.js
+++ api/quiz/quizRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { createQuiz, getQuizById, deleteQuiz } from "./quizController.js";
 import questionRouter from "../questions/questionRouter.js"
+import answerRouter from "../answers/answerRouter.js"
 
 const quizRouter = Router();
 
@@ -8,8 +9,11 @@ const quizRouter = Router();
 quizRouter.post("/", createQuiz); // api/quizzes
 // Route to Get quiz with id (questions and options)
 quizRouter.get("/:quizId", getQuizById) // Get api/quizzes/:quizId
+// Route to Delete quiz with id (questions and options)
+quizRouter.delete("/:quizId", deleteQuiz); //DELETE /api/quizzes/:quizId
 // Mount questionRouter to handle all question-related routes for a specific quiz
 quizRouter.use("/:quizId/questions", questionRouter)
-quizRouter.delete("/:quizId", deleteQuiz); //DELETE /api/quizzes/:quizId
+// Mount answerRouter to handle all answer-related routes for a specific quiz
+quizRouter.use("/:quizId/answers",answerRouter)
 
 export default quizRouter;
