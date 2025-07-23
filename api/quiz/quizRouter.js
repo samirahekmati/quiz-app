32067ed5bed3cb2f8de5fcc381cb77c7f6553581
Samira <samirahekmati@gmail.com>
2025-07-23 09:53:50 +0100

    feat: add PUT /api/quizzes/:quizId route to update quizzes

--- api/quiz/quizRouter.js
+++ api/quiz/quizRouter.js
@@ -4,7 +4,12 @@ import answerRouter from "../answers/answerRouter.js";
 import { mentorAuthMiddleware } from "../middleware/mentorAuthMiddleware.js";
 import questionRouter from "../questions/questionRouter.js";
 
-import { createQuiz, getQuizById, deleteQuiz } from "./quizController.js";
+import {
+	createQuiz,
+	getQuizById,
+	deleteQuiz,
+	updateQuiz,
+} from "./quizController.js";
 
 const quizRouter = Router();
 
@@ -14,6 +19,8 @@ quizRouter.post("/", mentorAuthMiddleware, createQuiz); // api/quizzes
 quizRouter.get("/:quizId", getQuizById); // Get api/quizzes/:quizId
 // Route to Delete quiz with id (questions and options)
 quizRouter.delete("/:quizId", deleteQuiz); // DELETE /api/quizzes/:quizId
+// Route to update quiz with id
+quizRouter.put("/:quizId", updateQuiz); // PUT /api/quizzes/:quizId
 // Mount questionRouter to handle all question-related routes for a specific quiz
 quizRouter.use("/:quizId/questions", questionRouter);
 // Mount answerRouter to handle all answer-related routes for a specific quiz
