11d5ec515203fdbd81738b8b31c5f71d4edf6883
Samira <samirahekmati@gmail.com>
2025-08-08 08:53:25 +0100

    feat(auth): add /mentor route to fetch logged-in mentor details

--- api/auth/authRouter.js
+++ api/auth/authRouter.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
-import { handleLogin, handleRegister } from "./authController.js";
+import { mentorAuthMiddleware } from "../middleware/mentorAuthMiddleware.js";
+
+import {
+	handleLogin,
+	handleRegister,
+	getMentorDetails,
+} from "./authController.js";
 
 const router = Router();
 
 router.post("/login", handleLogin);
 router.post("/register", handleRegister);
+router.get("/mentor", mentorAuthMiddleware, getMentorDetails);
 
 export default router;
